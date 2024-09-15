82beb4500b64fce8b39f5984ad6a3351ab934957
Parsa <parsa82ab@gmail.com>
2024-09-15 14:59:43 +0330

    add user signup

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import User
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.forms import ValidationError
 
 
 class UserCreationForm(forms.ModelForm):
@@ -39,3 +40,16 @@ class UserChangeForm(forms.ModelForm):
 class LoginForm(forms.Form):
     email = forms.CharField(label="Email/Username")
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class SignupForm(forms.Form):
+    email = forms.CharField()
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Confirm Password", widget=forms.PasswordInput)
+
+    def clean_password2(self):
+        pw1 = self.cleaned_data["password1"]
+        pw2 = self.cleaned_data["password2"]
+        if pw1 and pw2 and pw1 != pw2:
+            raise ValidationError("Passwords must match.")
+        return pw2
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.views.generic import FormView, View
-from .forms import LoginForm
+from .forms import LoginForm, SignupForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import get_user_model
 
 
 class LoginView(FormView):
@@ -22,4 +23,17 @@ class LogoutView(View):
     def get(self, request):
         if request.user.is_authenticated:
             logout(request)
+        return redirect("home")
+
+
+class SignupView(FormView):
+    form_class = SignupForm
+    template_name = "accounts/signup.html"
+
+    def form_valid(self, form):
+        cd = form.cleaned_data
+        user = get_user_model().objects.create_user(
+            email=cd["email"], password=cd["password1"]
+        )
+        login(self.request, user)
         return redirect("home")
