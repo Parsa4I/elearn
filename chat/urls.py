d36e7141ce62619d9edbab3caf2bb4f46ece7b7f
Parsa <parsa82ab@gmail.com>
2024-09-22 18:50:45 +0330

    add course-specific chat room templates

--- chat/urls.py
+++ chat/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = "chat"
 
 urlpatterns = [
-    path("", views.ChatRoomView.as_view(), name="chat_room"),
+    path("<slug:slug>/", views.ChatRoomView.as_view(), name="chat_room"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,16 @@
-from django.views.generic import TemplateView
+from django.views.generic.base import TemplateResponseMixin
+from django.views.generic import View
+from courses.models import Course
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseForbidden
 
 
-class ChatRoomView(TemplateView):
+class ChatRoomView(LoginRequiredMixin, TemplateResponseMixin, View):
     template_name = "chat/chat_room.html"
+
+    def get(self, request, slug):
+        course = get_object_or_404(Course, slug=slug)
+        if course not in request.user.courses_joined.all():
+            return HttpResponseForbidden()
+        return self.render_to_response({"course": course})
